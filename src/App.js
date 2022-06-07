c97f18773f7462bdbf2e5e397c23fa5a652325e3
PalomaSelva <palomaselva1@gmail.com>
2022-06-07 20:42:40 -0300

    Github3

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ const App = ()=> {
     return (
         <SafeAreaView style={style.container}>
             <StatusBar backgroundColor={'#14161c'} barStyle="light-content"></StatusBar>
-            <View>
+            <View style={style.content}>
                 <Image source={{uri:imageProfile}} style={style.avatar} accessibilityLabel="Paloma Selva" />  
-                <Text style={style.usuario}>Paloma Selva</Text>
+                <Text style={[style.usuario, style.defaultFont]}>Paloma Selva</Text>
+                <Text style={[style.nickname, style.defaultFont]}>PalomaSelva</Text>
+                <Text style={[style.bio, style.defaultFont,]}>Web developer / Mobile developer - HTML | CSS | JS | React-Native</Text>
+
             </View>
         </SafeAreaView>
     );
@@ -18,24 +21,48 @@ const App = ()=> {
 export default App
 
 const style = StyleSheet.create({
-    container: {
+    container: { //Coluna 
     backgroundColor:'#0A0C10',
     flex:1,
-},  
+    justifyContent:'center',
+    },  
+
     avatar:{
-    height:350,
-    width:350,
+    height:300,
+    width:300,
     borderRadius:300,
     borderColor:'white',
     borderWidth:3,
+    /*CENTRALIZAR IMG
     marginLeft:'auto',
     marginRight:'auto',
-},
+    */
+    },
+
+    content:{
+    alignItems:'center',
+    
+    },
 
     usuario:{
-    color:'white',
     fontSize:30,
     fontWeight:'bold',
     marginTop:10,
+   
+    }, 
+
+    defaultFont:{
+    color:'white',
+    },
+
+    nickname:{
+    fontSize:22,
+    color:'#F0F3F6',
+    fontWeight:'300',
+    },
+
+    bio:{
+    fontSize:18,
+    fontWeight:'300',
 },
 })
